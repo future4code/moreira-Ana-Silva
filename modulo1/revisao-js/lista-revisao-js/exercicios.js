3efc3bb905c868d25e832a1284e909f208d23e84
AnaKarine27 <karinesantos364@gmail.com>
2021-12-13 21:32:08 -0300

    revisao-JS

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -35,42 +35,96 @@ function retornaNumerosPares(array) {
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
-    
+    const numerosParesArray = array.filter((par) => {
+        if (par % 2 === 0) {
+            return par
+        }
+    });
+    const duplicar = numerosParesArray => numerosParesArray ** 2;
+    return numerosParesArray
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-
+    let maiorNumero = 0;
+    for (let index of array) {
+        if (maiorNumero < index) {
+            maiorNumero = index
+        }
+    }
+    return maiorNumero
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
+    const maior = Math.max(num1, num2);
+    const menor = Math.min(num1, num2);
 
+    function maiorDivididoMenor() {
+        if (maior % menor === 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    const diferenca = maior - menor;
+
+    const objetoNumeros = {
+        maiorNumero: maior,
+        maiorDivisivelPorMenor: maiorDivididoMenor(),
+        diferenca: diferenca
+    };
+    return objetoNumeros
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
+    const arrayDePares = []
 
+    for (let contador = 0; arrayDePares.length < n; contador++) {
+        if (contador % 2 === 0) {
+            arrayDePares.push(contador)
+        }
+    }
+    return arrayDePares
 }
+
+console.log(retornaNumerosPares(3))
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+    if
+        (ladoA === ladoB && ladoA === ladoC) {
+        return "Equilátero"
+    } else if (ladoA === ladoB || ladoA === ladoC || ladoB === ladoC) {
+        return "Isósceles"
+    } else
+        return "Escaleno"
 }
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
 
 }
-
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-
+    const assistirFilme = {
+        nome: 'O Diabo Veste Prada',
+        ano: 2006,
+        diretor: 'David Frankel',
+        atores: ['Meryl Streep', 'Anne Hathaway', 'Emily Blunt', 'Stanley Tucci']
+    }
+    return (`Venha assistir ao filme ${assistirFilme.nome}, de ${assistirFilme.ano}, dirigido por ${assistirFilme.diretor} e estrelado por ${assistirFilme.atores[0]}, ${assistirFilme.atores[1]}, ${assistirFilme.atores[2]}, ${assistirFilme.atores[3]}.`)
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-
+    const novoObjeto = {
+    ...pessoa, 
+    nome: "ANÔNIMO",
+    }
+    return novoObjeto
 }
 
 // EXERCÍCIO 13A
