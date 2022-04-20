cf7231f19cb3b743a62107e38b88e283ac01b953
AnaKarine27 <karinesantos364@gmail.com>
2022-04-19 21:36:35 -0300

    heranca

---
+++ modulo5/heranca-polimorfismo/src/index.ts
@@ -0,0 +1,24 @@
+
+//HERANÇA
+
+//EXERCÍCIO 01
+//A) Não será possível pois ela está como private só seria possível se ela tivesse atribuida a um setor.
+//B) Foi impressa apenas uma vez.
+
+//EXERCÍCIO 02
+//A) Foi impressa apenas uma vez.
+//B) Duas vezes, pois tem o primeiro newUser e o segundo no Customer.
+
+//EXERCÍCIO 03
+//A) Não, pois é um atributo privado só seria possível colocando um metodo get dentro do User.
+
+//EXERCÍCIO 04
+//A) customer.introduceYourself()
+
+//EXERCÍCIO 05
+/*
+A) public interoduceYourself(): string {
+      return `Olá, sou ${this.name}. Bom dia!`;
+    }
+}
+*/
