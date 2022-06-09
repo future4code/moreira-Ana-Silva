2e60b4494b9c0b0981073465399f5fb5d04413e3
AnaKarine27 <karinesantos364@gmail.com>
2022-06-09 14:20:03 -0300

    FIX: Resolvido bug na renderização

--- modulo6/rodada-cases-semana4/brainn-app/src/Pages/HomePage/HomePage.js
+++ modulo6/rodada-cases-semana4/brainn-app/src/Pages/HomePage/HomePage.js
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from 'react'
-import useRequestData from '../../Hooks/UseRequestData'
-import { BASE_URL } from '../../Constants/Urls'
+import React from 'react'
 import axios from 'axios'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 import Logo from '../../Assets/logo.png'
 import { colors } from '../../Constants/Colors'
+import { BASE_URL } from '../../Constants/Urls'
+import useRequestData from '../../Hooks/UseRequestData'
 import Background from '../../Components/Background/Background'
 import { Container, Main, SectionRight, Info, Select, LogoAlign, Footer, P } from "./Styled"
 
 const HomePage = () => {
   const [lotteries] = useRequestData([], `${BASE_URL}/loterias`)
   const [contests] = useRequestData([], `${BASE_URL}/loterias-concursos`)
-  const [raffleId, setRaffleId] = useState([])
-  const [selectValue, setSelectValue] = useState('0')
+  const [raffleId, setRaffleId] = React.useState([])
+  const [selectValue, setSelectValue] = React.useState('0')
 
   const contestsId = contests.filter((lot) => lot.loteriaId == selectValue).map((lot) => lot.concursoId)
   const color = colors.filter((lot) => lot.id == selectValue).map((lot) => lot.color)
 
-  useEffect(() => {
+  React.useEffect(() => {
     axios
-      .get(`${BASE_URL}/concursos/${contestsId[0]}`)
+      .get(`${BASE_URL}/concursos/${contestsId?.[0]?.contestsId === undefined ? 2359 : contestsId?.[0]?.contestsId}`)
       .then((response) => { setRaffleId([response.data]) })
       .catch((error) => { })
   }, [selectValue])
